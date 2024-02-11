e494648b04a148c5a0ce44b3476cceafe4f6b111
Firas GHRIBI <59181032+firasghribi@users.noreply.github.com>
2024-02-12 00:13:55 +0100

    Update quote-functions.js

--- quote-functions.js
+++ quote-functions.js
@@ -1,1 +1,62 @@
+export const HelperPrice = (totalHours, helperPrice, numberOfHelpers) => {
+  return (totalHours / 60) * helperPrice * numberOfHelpers;
+};
 
+export const farePrice = (quotePrice, farePriceRate) => {
+  return quotePrice * farePriceRate;
+};
+
+export const walkingDistancePrice = (
+  walkingMeterDistancePrice,
+  totalWalkingDistance,
+  totalCubicMeter
+) => {
+  return totalWalkingDistance * walkingMeterDistancePrice * totalCubicMeter;
+};
+
+export const levelPrice = (
+  totalCubicMeter,
+  cubicMeterPrice,
+  numberOfLevels,
+  levelCompoundRate
+) => {
+  return (
+    totalCubicMeter *
+    cubicMeterPrice *
+    Math.pow(1 + levelCompoundRate / 1, numberOfLevels) *
+    numberOfLevels
+  );
+};
+
+export const productCoefficient = (
+  totalCubicMeter,
+  productLoadingUnloadingRate
+) => {
+  return totalCubicMeter * productLoadingUnloadingRate;
+};
+
+export const distancePrice = (totalDistanceKm, distanceKmPrice) => {
+  return totalDistanceKm * distanceKmPrice;
+};
+
+export const quotePrice = (
+  walkingDistancePrice,
+  levelPrice,
+  distancePrice,
+  productPrice
+) => {
+  return (
+    walkingDistancePrice +
+    levelPrice +
+    distancePrice +
+  productPrice
+  );
+};
+
+export const checkMinimumPrice = (totalQuotePrice, minimumPrice) => {
+  if ( Number(totalQuotePrice) <  Number(minimumPrice)) {
+    return  Number(minimumPrice);
+  } else {
+    return  Number(totalQuotePrice);
+  }
+};
